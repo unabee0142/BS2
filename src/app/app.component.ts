4ee488ce451d9f8d017f3dfd02869a21935bc988
unabee0142 <mannot0142@gmail.com>
2021-11-23 16:35:59 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1: number = 5;
+  n2: number = 2;
+  picName:string= "https://www.buro247.my/thumb/670x830_0/images/CA_Nevertheless-Netflix-Kdrama-episode1-review.jpg";
+
+  hi(name:string){
+    alert("hi " + name);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { MenuComponent } from './template/menu/menu.component';
+import { BodyComponent } from './template/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
